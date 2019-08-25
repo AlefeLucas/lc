262270cd758ed09d5fd761dc8786089411ad7f14
Álefe Lucas <alefelucas.contato@gmail.com>
2019-08-25 04:50:03 -0300

    Necessário implementar tabela de símbolos e analisador léxico, não sei como fazê-los ainda.

---
+++ src/SymbolTable.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+
+/**
+ * TP1:
+ * 2- Implemente o tipo abstrato de dados “tabela de símbolos”, onde serão armazenados
+ * apenas as palavras reservadas e os identificadores. A tabela deverá armazenar
+ * registros que conterão, a princípio, campos para o número do token (byte) e o
+ * lexema (arranjo de caracteres) da palavra ou identificador. Devem ser
+ * implementadas duas funções:
+ *
+ * • Uma função que pesquisa a tabela em busca de um lexema e retorna o endereço do
+ * registro correspondente, caso exista, ou NULL se este não estiver na tabela.
+ * • Uma função que insere dinamicamente um registro na tabela, com o token e seu
+ * lexema, retornando o endereço de inserção.
+ *
+ * @see <link>https://stackoverflow.com/questions/14954721/what-is-the-difference-between-a-token-and-a-lexeme</link>
+ */
+public class SymbolTable {
+
+    public SymbolTable() {
+
+    }
+}
---
+++ src/SymbolTableSingleton.java
@@ -0,0 +1,12 @@
+@SuppressWarnings("unused")
+public abstract class SymbolTableSingleton {
+    private static SymbolTable symbolTable;
+
+    public static SymbolTable getInstance(){
+        if(symbolTable == null){
+            symbolTable = new SymbolTable();
+        }
+
+        return  symbolTable;
+    }
+}
